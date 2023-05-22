4f5c9b0bebabd02fd17bbbcc4d44749e8dd7cfc7
Matheus Marques <bob110.marques@gmail.com>
2023-05-22 19:36:47 -0300

    a

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http' 
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
+import { NgxPaginationModule } from 'ngx-pagination';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    PokemonListComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    NgxPaginationModule
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
